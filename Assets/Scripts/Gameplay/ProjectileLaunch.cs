0ad4296d3d63e9764ea6c66dbf6d4c60ad058f75
Ren Tamdang <tamdangren@gmail.com>
2026-01-27 13:40:20 +0800

    Updated input system and projectile launch script (WIP)

--- Assets/Scripts/Gameplay/ProjectileLaunch.cs
+++ Assets/Scripts/Gameplay/ProjectileLaunch.cs
@@ -2,34 +2,50 @@ using UnityEngine;
 
 public class ProjectileLaunch : MonoBehaviour
 {
-
+    private InputSystem_Actions inputActions;
     //* v (final velocity), u (initial velocity), a (acceleration), t (time)
     // v = u + at
 
     // Start is called before the first frame update
 
-    public float initialVelocity = 10f;
-    public float acceleration = 9.81f;
-    public float time = 2f;
-    private bool launched = false;
+    // public float initialVelocity = 10f;
+    // public float acceleration = 9.81f;
+    // public float time = 2f;
+    // private bool launched = false;
+
+    void Awake()
+    {
+        inputActions = new InputSystem_Actions();
+        inputActions.Player.Enable();
+    }
+
+    void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
+    void Start()
+    {
+        inputActions.Player.HoldForce.performed += ctx => CalculateForce();
+    }
+
+    // Click on ball and hold and drag to start calculating for force and direction of launch
+    void CalculateForce()
+    {
+        Debug.Log("Calculating Force and Direction for Launch");
+    }
+
+    void Update()
+    {
+
+    }
 
     void FixedUpdate()
     {
-        // Only launch once. Keep the finalVelocity calculation exactly as it was
-        if (launched) return;
-
-        Rigidbody rb = GetComponent<Rigidbody>();
-
-        if (rb == null) return;
-
-        float finalVelocity = initialVelocity + (acceleration * time);
-
-        rb.useGravity = true;
-
-        Vector3 impulse = transform.forward.normalized * finalVelocity * rb.mass;
-
-        rb.AddForce(impulse, ForceMode.Impulse);
-
-        launched = true;
     }
 }
